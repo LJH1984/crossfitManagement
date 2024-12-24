3aee36c1d105edf9ee849a6bfec94268ba5ab94c
LJH1984 <sentim@nate.com>
2024-12-24 14:13:54 +0900

    first commit

--- src/main/java/com/crossfitmanagement/util/EmailUtil.java
+++ src/main/java/com/crossfitmanagement/util/EmailUtil.java
@@ -5,8 +5,47 @@ import javax.mail.internet.*;
 import java.util.Properties;
 
 public class EmailUtil {
-    //이메일전송
-    public static void sendEmail(String to, String subject, String body) throws MessagingException{
+    /*
+     *  이메일 전송 메서드
+     * @param from 발신 이메일 주소
+     * @param password 발신 이메일 비밀번호
+     * @param to 수신 이메일 주소
+     * @param subject 이메일제목
+     * @param smtpHost SMTP 서버 호스트
+     * @param smtpPost SMTP 서버 포트
+     * @throws MessagingException  이메일 전송 중 오류 발생 시
+     */
+    public static void sendEmail(String from, String password, String to, String subject, String body, String smtpHost, String smtpPort) throws MessagingException {
+        //SMTP 이메일설정
+        Properties properties = new Properties();
+        properties.put("mail.smpt.auth", "ture");
+        properties.put("mail.smpt.starttls.enable", "true");
+        properties.put("mail.smpt.host", smtpHost);
+        properties.put("mail.smtp.prot", smtpPort);
+
+        //인증생성
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, password);
+            }
+        });
+
+        //이메일 메세제 작성
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(from)); //발실자 이메일 설정
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to)); //수신자설정
+        message.setSubject(subject); //제목설정
+        message.setText(body); //본문 설정
+
+        //이메일전송
+        Transport.send(message);
+
+        System.out.println("이메일이 전송되었습니다.");
 
     }
+
+
+
+
 }
--- src/main/java/com/crossfitmanagement/util/PasswordUtil.java
+++ src/main/java/com/crossfitmanagement/util/PasswordUtil.java
@@ -1,5 +1,24 @@
 package main.java.com.crossfitmanagement.util;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class PasswordUtil {
+
+    // AES 키 (최소 16바이트)
+    private static final String SECRET_KEY
+
+    //비밀번호를 SHA-256으로 암호화
+    public static String encryptPassword(String password) throws NoSuchAlgorithmException{
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte[] hashedBytes = md.digest(password.getBytes());
+        StringBuilder sb = new StringBuilder();
+
+        for(byte b : hashedBytes){
+            sb.append(String.format("%02x",b));
+        }
+        return sb.toString();
+    }
+
 }
+
